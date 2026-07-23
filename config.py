801716a93327464d0bca287def8f5680c5e89339
Kumar Sarthak <kumarsarthak0712@gmail.com>
2026-07-23 08:50:40 +0530

    feat: update config_loader type hint

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import tomllib
 from pathlib import Path
 
 
-def config_loader() -> dict:
+def config_loader() -> dict[str, dict]:
     with Path("config.toml").open("rb") as config_file:
         config = tomllib.load(config_file)
         return config
